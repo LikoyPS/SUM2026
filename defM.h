c66a0e287077c80af3b818ade2783eb9620e5fe6
LikoyPS <likoyps@yandex.ru>
2026-06-10 17:11:53 +0300

    Create defM.h

---
+++ defM.h
@@ -0,0 +1,41 @@
+/* FILE NAME: def.h
+ * PURPOSE: 3D math implementation module.
+ * PROGRAMMER: MI6
+<<<<<<< HEAD
+ * DATE: 09.06.2026
+=======
+ * DATE: 10.06.2026
+>>>>>>> 96e2e086243044462879a9bd4d5d515a3f93cdcc
+ */
+
+#ifndef __def_h_
+#define __def_h_
+
+/* Debug memory allocation support */
+#ifndef NDEBUG
+#  define _CRTDBG_MAP_ALLOC
+#  include <crtdbg.h>
+#  define SetDbgMemHooks() \
+     (_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_CHECK_ALWAYS_DF |        \
+       _CRTDBG_ALLOC_MEM_DF | _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG)),           \
+      _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG | _CRTDBG_MODE_FILE),   \
+      _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR),                      \
+      _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG | _CRTDBG_MODE_FILE),  \
+      _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR),                     \
+      _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_DEBUG | _CRTDBG_MODE_FILE), \
+      _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR))
+
+#else /* NDEBUG */
+#  define SetDbgMemHooks() ((void)0)
+#endif /* NDEBUG */
+#include <stdlib.h>
+
+#include "mth/mth.h"
+#include <windows.h>
+
+typedef long long INT64;
+typedef unsigned long long UINT64;  
+
+#endif /* __def_h_ */
+ 
+/* END OF 'def.h' FILE */
