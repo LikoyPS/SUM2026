66de20678a96e9eaa09e6dcb68f00416daac83d6
Клинских Илья <I.Klinskikh@ad.pml30.ru>
2026-06-15 23:07:55 +0300

    home 00 update

--- T00GUESS/t00guess.c
+++ T00GUESS/t00guess.c
@@ -1,22 +1,44 @@
-/* Klinskikh Ilya, 10-1 01.06.2026 */
-#include <stdio.h>
+/* Klinskikh Ilya, IK1 01.06.2026 */
 #include <windows.h>
 
-void main( void )
-{
-  int a = 0, b = 100, m;
-  unsigned char s[100];
+int AskQuestion(int mid) {
+    char message[256];
+    sprintf(message, "your number is more than %d?", mid);
+    return MessageBox(NULL, message, "guess the number", MB_YESNO | MB_ICONQUESTION);
+}
 
-  while (a < b)
-  {
-    if (MessageBox(NULL, "number...", "window", MB_OK) == IDOK)
-    {
-      m = (a + b) / 2;
-      sprintf(s, "number more than %i?", m);
-      if (MessageBox(NULL, s, "window", MB_YESNOCANCEL) == IDCANCEL)
-      {
-        MessageBox(NULL, "right", "window", MB_OK);
-      }
+void WinGame(int number) {
+    char message[256];
+    sprintf(message, "your number is - %d!", number);
+    MessageBox(NULL, message, "end", MB_OK | MB_ICONINFORMATION);
+}
+
+int main() {
+    int low = 1;      
+    int high = 100;   
+    int mid;          
+    int answer;       
+
+    MessageBox(NULL, "change the number from 1 to 100", "guess the number", MB_OK | MB_ICONINFORMATION);
+
+    while (low <= high) {
+        mid = (low + high) / 2;  
+
+        if (low == high) {
+            WinGame(low);
+            return 0;
+        }
+
+        answer = AskQuestion(mid);
+
+        if (answer == IDYES) {
+            low = mid + 1;
+        } else if (answer == IDNO) {
+            high = mid;
+        } else {
+            return 0;
+        }
     }
-  {
+
+    return 0;
 }
